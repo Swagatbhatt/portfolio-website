4e92eac4c9969d23b7e07ca330091235f39c50b6
Swagatbhatt <swagatbhatt6570@gmail.com>
2026-01-13 11:06:04 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,40 @@
 const CACHE_NAME = "portfolio-cache-v1";
+
 const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
-  "./script.js"
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install: cache essential files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Activate: remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve cache first, then network
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
